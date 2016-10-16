9e47ab3c1209b307fef12b3c0b3f4226b6639daf
savinovvu <savinov_vu@inbox.ru>
2016-10-16 17:54:18 +0800

    fix gradle build

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,22 +1,8 @@
-import org.junit.Test;
-import ru.inbox.savinov_vu.entity.SavedFile;
 import ru.inbox.savinov_vu.service.Service;
 
 /**
  * Created by skorpion on 08.10.16.
  */
 public class ServiceTest {
-
     Service service;
-
-    @Test
-    public void testCreate() {
-        SavedFile savedFile = new SavedFile("testИван", "777-777");
-        service.create(savedFile);
-        System.out.println(savedFile);
-    }
-
-
-
-
 }
