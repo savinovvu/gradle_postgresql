9731c3140c18312bd6acd1fa01a0b544c50af462
savinovvu <savinov_vu@inbox.ru>
2016-12-26 20:49:59 +0800

    add link response.

--- src/main/java/ru/inbox/savinov_vu/controller/PictureController.java
+++ src/main/java/ru/inbox/savinov_vu/controller/PictureController.java
@@ -34,7 +34,7 @@ public class PictureController {
 
         staticFileLocation("/public");
 
-
+        /*get verifier code and url from instagram*/
         get("/getCode", (request, response) -> {
 
             InstagramService instagramService = new InstagramAuthService()
